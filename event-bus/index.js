e7c10577009803d3971263396b675eb5b9cf0b77
Mario <3307245+mgarci201@users.noreply.github.com>
2021-12-04 21:17:40 +0000

    Event Sync test implement using an event handler

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,13 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+// Test event bus w/ array
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err =>  {
         console.log(err.message)}));
@@ -18,6 +23,10 @@ app.post('/events', (req, res) => {
         console.log(err.message)}));
 
     res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => console.log('Listening on 4005'));
