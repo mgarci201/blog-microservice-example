c43f5018393cb9c7e58d2fe621578828cdd9ee6c
Mario <3307245+mgarci201@users.noreply.github.com>
2021-12-04 18:50:03 +0000

    Add moderation feature for comments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # blog-microservice-example
-microservice blog example test
+
+This simple project shows how to create a microservice using React & Express Node.
+
+* Separate posts serivce
+* Separate comments service
+* Client as the React app
+* Separate query service
+* and Event Bus to store events
+
+To run the app go to each folder and run npm install then:
+* Go to the client folder and run `npm start`
+* Go to the query folder and run `npm start`
+* Go to the event-bus folder and run `npm start`
+* Go to the posts folder and run `npm start`
+* Go to the comments folder and run `npm start`
+
+
+* Start by creating a post
+* Refresh the page to see the post
+* Then create a comment
--- comments/index.js
+++ comments/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Generate a random string of characters for testing
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
@@ -21,7 +22,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     const comments = commentsByPostId[req.params.id] || [];
     // commentsByPostId[req.params.id] = commentsByPostId[req.params.id] || [];
 
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: 'pending'});
     commentsByPostId[req.params.id] = comments;
 
     await axios.post('http://localhost:4005/events', {
@@ -29,15 +30,38 @@ app.post('/posts/:id/comments', async (req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received event', req.body.type);
+
+    const {type, data} = req.body;
+
+    if(type === 'CommentModerated') {
+        const {id, postId, status, content} = data;
+        const comments = commentsByPostId[postId];
+        
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        });
+
+    }
+
     res.send({});
 });
 
